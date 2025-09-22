ce3941a16474d510d8cc5eca6e8105df52567523
Hong <hongjingjie05@gmail.com>
2025-09-22 23:16:49 +0800

    Added randomized movie recommendations: multiple movies for each genre and getRandomMovie()

--- main.cpp
+++ main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+string getRandomMovie(const vector<string>& movies){
+  int r = rand() % movies.size();
+  return movies[r];
+}
 
 int main(){
 
+  srand(static_cast<unsigned int>(time(nullptr)));
   int choice;
+
+  vector<string> action ={
+    "Extraction (2020) - A mercenary embarks on a rescue mission.",
+    "Mad Max: Fury Road (2015) - Post-apocalyptic chase.",
+    "John Wick (2014) - An ex-hitman seeks vengeance."
+  };
+  
+  vector<string> comedy = {
+    "Murder Mystery (2019) - A couple gets tangled in a mystery.",
+    "Always Be My Maybe (2019) - Childhood friends reconnect.",
+    "The Wrong Missy (2020) - A mistaken invitation leads to chaos."
+  };
+
+  vector<string> drama = {
+    "The Pursuit of Happyness (2006) - Based on true events.",
+    "Marriage Story (2019) - A couple navigates divorce and family struggles.",
+    "The Irishman (2019) - A mob hitman's life and regrets are explored."
+  };
+
+  vector<string> scifi = {
+    "Interstellar (2014) - Astronauts travel through a wormhole.",
+    "Dune: Part Two (2024) - Paul Atreides unites with the Fremen to wage war.",
+    "Arrival (2016) - A linguist tries to communicate with aliens."
+  };
+  
+  vector<string> horror = {
+    "The Conjuring (2013) - Paranormal investigators help a family.",
+    "Hereditary (2018) - A family unravels terrifying secrets after a loss.",
+    "Bird Box (2018) - Survivors face unseen creatures that drive people to madness."
+  };
+
   cout << "Welcome to the Netflix movie recommendation!" << endl;
   cout << "--------------------------------------------" << endl;
   cout << "Choose a movie genre" << endl;
@@ -16,27 +57,33 @@ int main(){
   do{
   cout << "You chose: ";
   cin >> choice;
-
+  
+  string picked;
   switch (choice){
     case 1: 
+      picked = getRandomMovie(action);
       cout << "You chose: Action " << endl;
-      cout << "Recommedation:Extraction (2020) - A mercenary embarks on a rescue mission." << endl;
+      cout << "Recommedation: " << picked << endl;
       break;
       case 2: 
+      picked = getRandomMovie(comedy);
       cout << "You chose: Comedy " << endl;
-      cout << "Recommendation: Murder Mystery (2019) - A couple gets tangled in a mystery." << endl;
+      cout << "Recommendation: " << picked << endl;
       break;
       case 3: 
+      picked = getRandomMovie(drama);
       cout << "You chose: Drama " << endl;
-      cout << "Recommendation: The Pursuit of Happyness (2006) - Based on true events." << endl;
+      cout << "Recommendation: " << picked << endl;
       break;
       case 4: 
+      picked = getRandomMovie(scifi);
       cout << "You chose: Sci-fi " << endl;
-      cout << "Recommendation: Interstellar (2014) - Astronauts travel through a wormhole." << endl;
+      cout << "Recommendation: " << picked << endl;
       break;
       case 5: 
+      picked = getRandomMovie(horror);
       cout << "You chose: Horror " << endl;
-      cout << "Recommendation: The Conjuring (2013) - Paranormal investigators help a family." << endl;
+      cout << "Recommendation: " << picked << endl;
       break;
       case 0: 
       cout << "Exiting program. Goodbye!" << endl;
