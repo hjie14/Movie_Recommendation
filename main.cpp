d671f94c0b4a6accd9d2a9787cfcc6d423ef09b6
Hong <hongjingjie05@gmail.com>
2025-09-22 22:57:41 +0800

    Added switch case: Recommendations for each genre

--- main.cpp
+++ main.cpp
@@ -15,4 +15,32 @@ int main(){
   cout << "0. Exit" << endl;
   cout << "You chose: ";
   cin >> choice;
+
+  switch (choice){
+    case 1: 
+      cout << "You chose: Action " << endl;
+      cout << "Recommedation:Extraction (2020) - A mercenary embarks on a rescue mission." << endl;
+      break;
+      case 2: 
+      cout << "You chose: Comedy " << endl;
+      cout << "Recommendation: Murder Mystery (2019) - A couple gets tangled in a mystery." << endl;
+      break;
+      case 3: 
+      cout << "You chose: Drama " << endl;
+      cout << "Recommendation: The Pursuit of Happyness (2006) - Based on true events." << endl;
+      break;
+      case 4: 
+      cout << "You chose: Sci-fi " << endl;
+      cout << "Recommendation: Interstellar (2014) - Astronauts travel through a wormhole." << endl;
+      break;
+      case 5: 
+      cout << "You chose: Horror " << endl;
+      cout << "Recommendation: The Conjuring (2013) - Paranormal investigators help a family." << endl;
+      break;
+      case 0: 
+      cout << "Exiting program. Goodbye!" << endl;
+      default:
+      cout << "Invalid choice. Please enter a number (1-5)." << endl;
+  }
+  
 }
