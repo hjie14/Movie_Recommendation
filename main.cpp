f85349e8bdaa1436c118090f7d4225ddd8b1bde5
Hong <hongjingjie05@gmail.com>
2025-09-22 22:51:45 +0800

    Added genre list and input choice

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main(){
 
+  int choice;
   cout << "Welcome to the Netflix movie recommendation!" << endl;
   cout << "--------------------------------------------" << endl;
+  cout << "Choose a movie genre" << endl;
+  cout << "1. Action" <<endl;
+  cout << "2. Comedy" <<endl;
+  cout << "3. Drama" <<endl;
+  cout << "4. Sci-fi" <<endl;
+  cout << "5. Horror" <<endl;
+  cout << "0. Exit" << endl;
+  cout << "You chose: ";
+  cin >> choice;
 }
